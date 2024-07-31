f64c1c8d3e75fcb4e13a49d5f8972c4e40178d78
benoitivars <143166241+benoitivars@users.noreply.github.com>
2024-07-31 17:07:11 +0200

    15e commit première version du tuto, fin de la rédaction du tutoriel sur le débogage.

--- TUTO-DEBOGAGE/ExempleErreurLogique.java
+++ TUTO-DEBOGAGE/ExempleErreurLogique.java
@@ -3,7 +3,7 @@ public class ExempleErreurLogique {
         int nombre = 10;
         System.out.println("Le nombre a pour valeur " + nombre);
         boolean estPair = (nombre % 2 == 0);
-        nombre++;
+        ++nombre;
 
         if (estPair) {
             System.out.println("Le nombre " + nombre +  " est pair.");
